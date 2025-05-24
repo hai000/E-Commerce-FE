b58daf72f6ce69525be4c27072c13f13a6840c42
21130343 <21130343@st.hcmuaf.edu.vn>
2025-05-24 19:05:01 +0700

    add side bar

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -2,12 +2,17 @@ import { APP_NAME } from '@/lib/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import Menu from './menu'
-import { Button } from '@/components/ui/button'
-import { MenuIcon } from 'lucide-react'
 import data from '@/lib/data'
 import Search from './search'
+import Sidebar from "@/components/shared/header/sidebar";
+import {getAllCategories} from "@/lib/api/category";
+import {Category} from "@/lib/response/category";
 
-export default function Header() {
+export default async function Header() {
+    let categories = await getAllCategories()
+    if (typeof categories === "string") {
+        categories = []
+    }
     return (
         <header className='bg-orange-600  text-white'>
             <div className='px-2'>
@@ -36,13 +41,7 @@ export default function Header() {
                 </div>
             </div>
             <div className='flex items-center px-3 mb-[1px]  bg-orange-500'>
-                <Button
-                    variant='ghost'
-                    className='header-button flex items-center gap-1 text-base [&_svg]:size-6'
-                >
-                    <MenuIcon />
-                    All
-                </Button>
+                <Sidebar categories={categories as Category[]} />
                 <div className='flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]'>
                     {data.headerMenus.map((menu) => (
                         <Link

178f7353edd91c6e2522a07be40a859ef380e49c
Quang Hieu <126405081+quangHieu2109@users.noreply.github.com>
2025-06-16 12:49:08 +0700

    validation real-time sign-in and sign-up

--- src/app/[locale]/(auth)/sign-up/signup-form.tsx
+++ src/app/[locale]/(auth)/sign-up/signup-form.tsx
@@ -14,8 +14,7 @@ import {
     register,
     signInWithCredentials,
     validatePhoneNumber,
-    validEmail,
-    validPhoneNumber
+    validEmail
 } from "@/lib/api/user";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useTranslations } from "next-intl";
@@ -47,33 +46,44 @@ export default function SignUpForm() {
     const form = useForm<IUserRegisterRequest>({
         resolver: zodResolver(getUserSignUpSchema(t)),
         defaultValues: signUpDefaultValues,
-    })
+    });
 
-    // const { control, handleSubmit } = form;
-    const { control, handleSubmit, setError, clearErrors } = form;
-    // Hàm kiểm tra email qua API
+    const { control, handleSubmit, setError, clearErrors, formState: { errors } } = form;
+
     const validateEmail = async (email: string) => {
         try {
             const res = await validEmail(email);
             if (res.success === false) {
-                setError('email', { type: 'manual', message: res.data }); // Hiển thị thông báo lỗi từ API
-            }
-        } catch (error) {
-            console.error('Lỗi khi kiểm tra email:', error);
-        }
-    };
-    const validPhoneNumber = async (phoneNumber: string) => {
-        try {
-            const res = await validatePhoneNumber(phoneNumber);
-            if (res.success === false) {
-                setError('phoneNumber', { type: 'manual', message: res.data }); // Hiển thị thông báo lỗi từ API
+                setError('email', { type: 'manual', message: res.data });
             }
         } catch (error) {
             console.error('Lỗi khi kiểm tra email:', error);
         }
     };
 
+    const validPhoneNumber = async (phoneNumber: string) => {
+        try {
+            const res = await validatePhoneNumber(phoneNumber);
+            if (res.success === false) {
+                setError('phoneNumber', { type: 'manual', message: res.data });
+            }
+        } catch (error) {
+            console.error('Lỗi khi kiểm tra số điện thoại:', error);
+        }
+    };
+
     const onSubmit = async (data: IUserRegisterRequest) => {
+        // Kiểm tra xem có lỗi nào không
+        if (Object.keys(errors).length > 0) {
+            // Hiển thị thông báo lỗi nếu có lỗi
+            toast({
+                title: t('Toast.Error'),
+                description: t('Có lỗi xảy ra, vui lòng kiểm tra lại thông tin.'),
+                variant: 'destructive',
+            });
+            return; // Không gửi form nếu có lỗi
+        }
+
         const res = await register(data);
         if (typeof res === 'string') {
             toast({
@@ -128,11 +138,11 @@ export default function SignUpForm() {
                                         placeholder={t('Placeholder.Enter username')}
                                         {...field}
                                         onChange={(e) => {
-                                            field.onChange(e); // Cập nhật giá trị
+                                            field.onChange(e);
                                             if (e.target.value.length < 5) {
                                                 setError('username', { type: 'manual', message: t('Login.Username invalid') });
                                             } else {
-                                                clearErrors('username'); // Xóa lỗi nếu hợp lệ
+                                                clearErrors('username');
                                             }
                                         }}
                                     />
@@ -154,13 +164,13 @@ export default function SignUpForm() {
                                         placeholder={t("Placeholder.Enter email")}
                                         {...field}
                                         onChange={(e) => {
-                                            field.onChange(e); // Cập nhật giá trị
+                                            field.onChange(e);
                                             const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
                                             if (!emailPattern.test(e.target.value)) {
                                                 setError('email', { type: 'manual', message: t('email_invalid') });
                                             } else {
-                                                clearErrors('email'); // Xóa lỗi nếu hợp lệ
-                                                validateEmail(e.target.value); // Kiểm tra email đã tồn tại
+                                                clearErrors('email');
+                                                validateEmail(e.target.value);
                                             }
                                         }}
                                     />
@@ -181,14 +191,14 @@ export default function SignUpForm() {
                                         placeholder={t('Placeholder.Enter phone number')}
                                         {...field}
                                         onChange={(e) => {
-                                            field.onChange(e); // Cập nhật giá trị
+                                            field.onChange(e);
                                             const PHONE_REGEX_1 = /^0\d{9}$/;
                                             const PHONE_REGEX_2 = /^\+\d{2} \d{9}$/;
                                             if (!PHONE_REGEX_1.test(e.target.value) && !PHONE_REGEX_2.test(e.target.value)) {
                                                 setError('phoneNumber', { type: 'manual', message: t('phone number invalid') });
                                             } else {
-                                                clearErrors('phoneNumber'); // Xóa lỗi nếu hợp lệ
-                                                validPhoneNumber(e.target.value); // Kiểm tra email đã tồn tại
+                                                clearErrors('phoneNumber');
+                                                validPhoneNumber(e.target.value);
                                             }
                                         }}
                                     />
@@ -210,11 +220,15 @@ export default function SignUpForm() {
                                         placeholder={t('Placeholder.Enter password')}
                                         {...field}
                                         onChange={(e) => {
-                                            field.onChange(e); // Cập nhật giá trị
-                                            if (e.target.value.length < 6) {
+                                            field.onChange(e);
+                                            const passwordValue = e.target.value;
+                                            if (passwordValue.length < 6) {
                                                 setError('password', { type: 'manual', message: t('Login.Password invalid') });
                                             } else {
-                                                clearErrors('password'); // Xóa lỗi nếu hợp lệ
+                                                clearErrors('password');
+                                            }
+                                            if (passwordValue === form.getValues('confirmPassword')) {
+                                                clearErrors('confirmPassword');
                                             }
                                         }}
                                     />
@@ -236,11 +250,11 @@ export default function SignUpForm() {
                                         placeholder={t('User.Confirm Password')}
                                         {...field}
                                         onChange={(e) => {
-                                            field.onChange(e); // Cập nhật giá trị
+                                            field.onChange(e);
                                             if (e.target.value !== form.getValues('password')) {
                                                 setError('confirmPassword', { type: 'manual', message: t('User.Confirm Password incorrect') });
                                             } else {
-                                                clearErrors('confirmPassword'); // Xóa lỗi nếu hợp lệ
+                                                clearErrors('confirmPassword');
                                             }
                                         }}
                                     />
